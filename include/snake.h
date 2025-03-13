84c30a28317c8e45b2036e94e7052fd0e7fd9915
Leonardo Marinheiro <souleonardo1@gmail.com>
2025-03-13 10:46:45 -0300

    Enhanced redibility of snake code

--- include/snake.h
+++ include/snake.h
@@ -7,4 +7,5 @@ typedef struct snake_part_
     int radius;
 } snake_part;
 
-void move_snake(snake_part snake[], int snake_size, float x, float y, float forwad_x, float forwad_y);
+void move_snake(snake_part snake[], int snake_size, 
+    float _x, float _y, float _forwad_x, float _forwad_y);
--- src/snake.c
+++ src/snake.c
@@ -8,45 +8,59 @@ extern int game_is_running;
 
 void stick_part_to(const snake_part *fix, snake_part *to_join)
 {
-    float vector_x = (fix -> x) - (to_join -> x);
-    float vector_y = (fix -> y) - (to_join -> y);
+    // join_dir = fix.pos - to_join.pos
+    float join_dir_x = (fix -> x) - (to_join -> x);
+    float join_dir_y = (fix -> y) - (to_join -> y);
     
-    int desired_distance = (to_join -> radius) + (fix -> radius);
-    float vector_size = sqrt(vector_x*vector_x + vector_y*vector_y);
+    float join_dir_norm = sqrt(join_dir_x*join_dir_x + join_dir_y*join_dir_y);
 
-    vector_x /= vector_size;
-    vector_y /= vector_size;
+    // join_dir = join_dir/||join_dir||
+    join_dir_x /= join_dir_norm;
+    join_dir_y /= join_dir_norm;
+    // join_dir is now a versor
 
-    float spine_cos = (vector_x)*(fix->forwad_x) + (vector_y)*(fix->forwad_y);
+    // float join_dot_forwad = join_dir * fix.forward
+    float join_dot_forwad = (join_dir_x)*(fix->forwad_x) + (join_dir_y)*(fix->forwad_y);
     
-    if(spine_cos < cos(MAX_ANGLE)) 
+    if(join_dot_forwad < cos(MAX_ANGLE)) 
     {
-        float cross = (fix->forwad_x)*(vector_y) - (vector_x)*(fix->forwad_y);
+        // float cross_z = (fix.forwad x vector).z
+        float cross_z = (fix->forwad_x)*(join_dir_y) - (fix->forwad_y)*(join_dir_x);
 
-        if(cross > 0) 
+        if(cross_z > 0) 
         {
-            vector_x = cos(MAX_ANGLE)*(fix->forwad_x) - sin(MAX_ANGLE)*(fix->forwad_y);
-            vector_y = sin(MAX_ANGLE)*(fix->forwad_x) + cos(MAX_ANGLE)*(fix->forwad_y);
+            // to_join = rotate(fix.forward, MAX_ANGLE)
+            join_dir_x = cos(MAX_ANGLE)*(fix->forwad_x) - sin(MAX_ANGLE)*(fix->forwad_y);
+            join_dir_y = sin(MAX_ANGLE)*(fix->forwad_x) + cos(MAX_ANGLE)*(fix->forwad_y);
         }
-        else if(cross < 0)
+        else if(cross_z < 0)
         {
-            vector_x = cos(-MAX_ANGLE)*(fix->forwad_x) - sin(-MAX_ANGLE)*(fix->forwad_y);
-            vector_y = sin(-MAX_ANGLE)*(fix->forwad_x) + cos(-MAX_ANGLE)*(fix->forwad_y);
+            // to_join = rotate(fix.forward, -MAX_ANGLE)
+            join_dir_x = cos(-MAX_ANGLE)*(fix->forwad_x) - sin(-MAX_ANGLE)*(fix->forwad_y);
+            join_dir_y = sin(-MAX_ANGLE)*(fix->forwad_x) + cos(-MAX_ANGLE)*(fix->forwad_y);
         }
     }
-    to_join -> forwad_x = vector_x;
-    to_join -> forwad_y = vector_y;
+    // to_join.forward = join_dir
+    to_join -> forwad_x = join_dir_x;
+    to_join -> forwad_y = join_dir_y;
 
-    to_join -> x = (fix -> x) - desired_distance * (vector_x);
-    to_join -> y = (fix -> y) - desired_distance * (vector_y);
+    int desired_distance = (to_join -> radius) + (fix -> radius);
+
+    // to_join.pos = fix.pos - join_dir
+    to_join -> x = (fix -> x) - desired_distance * (join_dir_x);
+    to_join -> y = (fix -> y) - desired_distance * (join_dir_y);
 }
-void move_snake(snake_part snake[], int snake_size, float x, float y, float forwad_x, float forwad_y)
+void move_snake(snake_part snake[], int snake_size, 
+    float _x, float _y, float _forwad_x, float _forwad_y)
 {
-    snake[0].x = x;
-    snake[0].y = y;
+    // snake[0].pos = _pos
+    snake[0].x = _x;
+    snake[0].y = _y;
 
-    snake[0].forwad_x = forwad_x;
-    snake[0].forwad_y = forwad_y;
+    // snake[0].forwad = _forwad
+    snake[0].forwad_x = _forwad_x;
+    snake[0].forwad_y = _forwad_y;
 
-    for(int i = 0; i < snake_size-1; i++) stick_part_to(&snake[i],&snake[i+1]);
+    for(int i = 0; i < snake_size-1; i++) 
+        stick_part_to(&snake[i],&snake[i+1]);
 }
